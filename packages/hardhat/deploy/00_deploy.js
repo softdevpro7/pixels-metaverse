6b9ebb1e7d0080db60ba77bada4f052f51aa3e01
softdevpro7 <baptiste.3pierre@gmail.com>
2022-02-27 16:43:15 +0800

    update hardhat

--- packages/hardhat/deploy/00_deploy.js
+++ packages/hardhat/deploy/00_deploy.js
@@ -3,26 +3,28 @@ const { ethers } = require("hardhat");
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  console.log(deployer,"deployer")
 
-  await deploy("PMT721", {
+  const PMT721 = await deploy("PMT721", {
     from: deployer,
     log: true,
-    waitConfirmations: 5,
   });
+  console.log("PMT721", PMT721.address)
 
   // Getting a previously deployed contract
   const PMT721Contract = await ethers.getContract("PMT721", deployer);
 
-  await deploy("PixelsMetaverse", {
+  const PixelsMetaverse = await deploy("PixelsMetaverse", {
     from: deployer,
     args: [PMT721Contract.address],
     log: true,
-    waitConfirmations: 5,
   });
+  console.log("PixelsMetaverse", PixelsMetaverse.address)
 
   // Getting a previously deployed contract
   const PixelsMetaverseContract = await ethers.getContract("PixelsMetaverse", deployer);
 
   await PMT721Contract.setMinter(PixelsMetaverseContract.address);
+  console.log("deploy success")
 };
 module.exports.tags = ["PMT721"];
